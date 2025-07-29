2e51499f40b230f7b21760698d02211a79eb80a1
Michael Bai <michaelbai30@gmail.com>
2025-07-28 21:39:43 -0500

    added comments

--- stocks/server/app.py
+++ stocks/server/app.py
@@ -1,6 +1,6 @@
-# init backend server
+# Flask Web Server
 # define the URLs the app can respond to
-# map URLs to functions in logic.py
+# map URLs to functions defined in logic.py
 # receive data from frontend and returns json
 
 import os
@@ -23,57 +23,55 @@ from logic import(
 app = Flask(__name__, static_folder='frontend/static', static_url_path='/static', template_folder='frontend/templates')
 CORS(app) 
 
+# app route decorators define API endpoints (URLS)
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
+# return JSON containing portfolio total value, cash balance, and allocations
 @app.route('/api/summary', methods=['GET'])
 def summary():
+    # jsonify converts Python dict to JSON http
     return jsonify(get_portfolio_summary())
 
+# get portfolio data as a json
 @app.route('/api/portfolio', methods=['GET'])
 def portfolio():
     run_limit_checks() # auto-trigger any queued orders before returning data
-    # jsonify converts Python dict to JSON http
     return jsonify(get_portfolio_data()) 
 
+# handles request to /api/price
 @app.route('/api/price', methods=['GET'])
 def price():
-    symbol = request.args.get("symbol") 
-    return jsonify(get_price_json(symbol))
+    ticker = request.args.get("symbol") # get the ticker from the url and passs to get_price_json
+    return jsonify(get_price_json(ticker))
 
-
+# handles requests to buy stock
 @app.route('/api/buy', methods=['POST'])
 def buy():
     data = request.json # ex request.json {"symbol" : "AAPL", "shares" : 5}
     return jsonify(buy_stock(data['symbol'], int(data['shares'])))
 
+# handles requests to sell stock
 @app.route('/api/sell', methods=['POST'])
 def sell():
     data = request.json
     return jsonify(sell_stock(data['symbol'], int(data['shares'])))
 
-@app.route('/api/limit-order', methods=['POST'])
-def limit_order():
-    data = request.json
-    return jsonify(add_limit_order(
-        symbol=data['ticker'],
-        shares=int(data['shares']),
-        price=float(data['price']),
-        order_type=data['order_type']
-    ))
-
+# handles request to generate chart
 @app.route('/api/chart', methods=['GET'])
 def chart():
-    symbol = request.args.get('symbol')
+    ticker = request.args.get('symbol')
     period = request.args.get('period', '1mo') # default to one month
     outpath = os.path.join(app.static_folder, "chart.html")
     try:
-        path = generate_stock_chart(symbol, period, outpath)
+        path = generate_stock_chart(ticker, period, outpath)
         return jsonify({"image_path": path})
     except Exception as e:
         return jsonify({"error" : str(e)})
 
+# handles request to get price and percent change
 @app.route('/api/price/<ticker>', methods=['GET'])
 def get_price(ticker):
     try:
@@ -87,6 +85,7 @@ def get_price(ticker):
     except:
         return jsonify({"error" : "Invalid ticker"})
 
+# handles limit order requests
 @app.route('/api/limit-order', methods=['POST'])
 def place_limit_order():
     data = request.get_json()
@@ -97,14 +96,15 @@ def place_limit_order():
     price = float(data.get('price', 0))
     order_type = data.get('order_type')  # should be either LB, SB, LS, SL
 
-    # condition checking
+    # validate inputs
     if not ticker or shares <= 0 or price <= 0 or order_type not in ['LB', 'SB', 'LS', 'SL']:
         return jsonify({"error" : "Invalid Input"})
 
     res = add_limit_order(ticker, shares, price, order_type)
 
     return jsonify(res)
-    
+
+# check for possible limit orders
 @app.route('/api/check-orders', methods=['GET'])
 def check_orders():
     run_limit_checks()
